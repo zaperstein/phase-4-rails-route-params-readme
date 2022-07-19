4987a7428a56b21600d059cc841baf0c28751dbf
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-19 18:17:47 -0500

    commit tree f9a0090b8d1d17feaa1985d8a70cfc0ce519cc30

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,11 @@
 class CheesesController < ApplicationController
-
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 end
